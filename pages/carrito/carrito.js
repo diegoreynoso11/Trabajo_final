2f326c79c058b05d77097807faafb2b3ae612213
apuu03 <57383340+apuu03@users.noreply.github.com>
2023-10-18 20:21:18 -0300

    funcionalidad de busqueda de elementos y de carrito agregadas

--- pages/carrito/carrito.js
+++ pages/carrito/carrito.js
@@ -1,6 +1,6 @@
-let carrito = JSON.parse(localStorage.getItem("carrito")) || {};
-
-
+//JSON.parse(localStorage.getItem("carrito")) ||
+let carrito =  {};
+const total = document.getElementById("total");
 function agregarAlCarrito(listItem) {
   var cartItem = {
     id: listItem.id,
@@ -8,7 +8,7 @@ function agregarAlCarrito(listItem) {
     price: listItem.querySelector("p").innerText,
     thumbnail: listItem.querySelector(".li-img-catalogo").src,
     quantity: 1,
-};
+  };
   if (carrito.hasOwnProperty(cartItem.id)) {
     cartItem.quantity = carrito[cartItem.id].quantity + 1;
   }
@@ -16,22 +16,38 @@ function agregarAlCarrito(listItem) {
   localStorage.setItem("carrito", JSON.stringify(carrito));
   // console.log(carrito)
 }
-
+let totalProducto = 0;
 for (let item in carrito) {
-    console.log(carrito[item].price);
-    const carritoLista = document.querySelector("#carrito");
-    const newLi = document.createElement("li");
-    newLi.innerHTML = `<li id="${carrito[item].id}"class=li-carrito>
-    <h2 class="h1-carrito">${carrito[item].title}</h2>
+  // console.log(carrito);
+  const carritoLista = document.querySelector("#carrito");
+  const newLi = document.createElement("li");
+  let subtotal = Number(carrito[item].price) * carrito[item].quantity;
+  totalProducto += subtotal;
+  console.log(carrito);
+  newLi.innerHTML = `
+                    <li id="${carrito[item].id}" class=li-carrito>
+                    <h2 class="h1-carrito">${carrito[item].title}</h2>
+                    <img class="li-img-carrito" src="${
+                      carrito[item].thumbnail
+                    }" title="${carrito[item].title}" alt="${
+    carrito[item].title
+  }">
             <p>$ ${carrito[item].price} </p>
-            <p>${carrito[item].quantity} </p>
+            <p>Cantidad : ${carrito[item].quantity} </p>
+            <div class="btn-sum-res">
+            <button>+</button>
+            <button>-</button>
+            </div>
+          <button class="btn-delete">Eliminar</button>
             
-            <img class="li-img-carrito" src="${carrito[item].thumbnail}" title="${carrito[item].title}" alt="${carrito[item].title}">
+            <p>Subtotal : ${
+              Number(carrito[item].price) * carrito[item].quantity
+            }</p>
             </li>
             `;
-            carritoLista.appendChild(newLi);
-  }
-function crearElementoCarrito() {
+  carritoLista.appendChild(newLi);
 }
-crearElementoCarrito()
+// total.innerHTML = `$ ${totalProducto}`;
+function crearElementoCarrito() {}
+crearElementoCarrito();
 export { agregarAlCarrito };
--- pages/catalogo/catalogo.js
+++ pages/catalogo/catalogo.js
@@ -1,6 +1,6 @@
-import { agregarAlCarrito } from '../carrito/carrito.js';
+import { agregarAlCarrito } from "../carrito/carrito.js";
 
-
+const h1ProductosCatalogo = document.querySelector(".h1-productos-catalogo")
 async function agregarElemento() {
   const catalogoLista = document.querySelector("#catalogo");
   try {
@@ -8,11 +8,47 @@ async function agregarElemento() {
     const dataBusqueda = await busqueda.json();
     //buscador por input
     const buscador = document.getElementById("buscador");
+    let timeout = null;
     buscador.addEventListener("input", (e) => {
-      const nwbusqueda = dataBusqueda.filter(
-        (item) => item.name === e.target.value
-      );
-      console.log(item.name)
+      clearTimeout(timeout);
+      timeout = setTimeout(async () => {
+        
+        catalogoLista.innerHTML = "";
+        if (e.target.value.trim() !== "") {
+          const nwbusqueda = dataBusqueda.filter((item) =>
+          item.name.toLowerCase().includes(e.target.value.toLowerCase())
+          );
+          console.log(nwbusqueda.length)
+          if (nwbusqueda.length === 0) {
+            h1ProductosCatalogo.innerHTML = "Ningun producto encontrado";
+          } else {
+            h1ProductosCatalogo.innerHTML = "Productos encontrados..";
+          }
+          for (let i = 0; i <= nwbusqueda.length; i++) {
+            const element = nwbusqueda[i];
+            const res = await fetch(
+              `https://api.mercadolibre.com/sites/MLA/search?q=${element.name}`
+              );
+              const dataRes = await res.json();
+            const newLi = document.createElement("li");
+            const newItem = dataRes.results[0];
+            newLi.innerHTML = `<li id="${newItem.id}"class=li-catalogo>
+        <h2 class="h1-catalogo">${element.name}</h2>
+        <strong>Precio:</strong><br/>
+        <div class="div-precio-catalogo">
+        <span>$</span><p>${newItem.price} </p>
+        </div>
+        <img class="li-img-catalogo" src="${newItem.thumbnail}" title="${newItem.title}" alt="${newItem.title}">
+        <div class="div-info-catalogo">
+        <button  class="button-icon info"><img src="../../statics/icons/info.svg" title="Informacion" alt="Informacion" class="icon-img"></button>
+        <button  class="cart button-icon"><img src="../../statics/icons/cart.svg" title="Agregar al carrito" alt="CarritoSvg" class="icon-img"></button>
+        </div>
+        </li>
+        `;
+            catalogoLista.appendChild(newLi);
+          }
+        }
+      }, 1000); 
     });
     for (let i = 0; i < 6; i++) {
       const response = await fetch(
@@ -23,12 +59,13 @@ async function agregarElemento() {
       const dataResponse = await response.json();
       const newItem = dataResponse.results[0];
       console.log(newItem);
-
       const newLi = document.createElement("li");
       newLi.innerHTML = `<li id="${newItem.id}"class=li-catalogo>
           <h2 class="h1-catalogo">${newItem.title}</h2>
-          <strong>Precio:</strong> 
-          <p>$ ${newItem.price} </p>
+          <strong>Precio:</strong><br/>
+          <div class="div-precio-catalogo">
+          <span>$</span><p>${newItem.price} </p>
+          </div>
           <img class="li-img-catalogo" src="${newItem.thumbnail}" title="${newItem.title}" alt="${newItem.title}">
           <div class="div-info-catalogo">
           <button  class="button-icon info"><img src="../../statics/icons/info.svg" title="Informacion" alt="Informacion" class="icon-img"></button>
@@ -42,18 +79,17 @@ async function agregarElemento() {
     const cart = document.querySelectorAll(".cart");
     info.forEach((btn, index) => {
       btn.addEventListener("click", (e) => {
-        console.log("carrito", index, e.target.closest('li'));
+        console.log("carrito", index, e.target.closest("li"));
       });
     });
     cart.forEach((btn, index) => {
       btn.addEventListener("click", (e) => {
-        var listItem = e.target.closest('li');
-        agregarAlCarrito(listItem)
-        
+        var listItem = e.target.closest("li");
+        agregarAlCarrito(listItem);
+      });
     });
-  });
-} catch (error) {
-  console.error(error);
-}
+  } catch (error) {
+    console.error(error);
+  }
 }
 agregarElemento();
